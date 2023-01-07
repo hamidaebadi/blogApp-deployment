8ecb6a00e1cc69c748502e3679d406bead69678b
Hamid Aebadi <hamidebadi1996@yahoo.com>
2023-01-07 11:43:37 +0200

    Add a simple pipeline CI

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-let PORT = process.env.PORT || 5000
+let PORT = process.env.PORT || 3001
 
 let MONGODB_URL = process.env.NODE_ENV === 'test'
 ? process.env.MONGODB_URL_TEST
